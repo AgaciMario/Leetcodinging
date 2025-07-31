f14d7b9eba9528ed68fa790595f1aa97232bfe1e
Agaci Mário <agacimnm@gmail.com>
2025-07-31 16:41:27 -0300

    Adicionando solução para o problema de remoção de valores duplicados em um array ordenado

---
+++ RemoveDuplicatesFromSortedArray/Program.cs
@@ -0,0 +1,44 @@
+﻿int RemoveDuplicates_notOtp(int[] nums) {
+    int lastIndex = nums.Length - 1;
+
+    // count the number of non-repeted values
+    int lastValueSeen = nums[0];
+    int k = 1;
+    for (int i = 1; i < nums.Length; i++)
+    {
+        if(nums[i] == lastValueSeen)
+            continue;
+        else {
+            lastValueSeen = nums[i];
+            k++;
+        }
+    }
+
+    // Adding a new max-value to the duplicated spaces in the array
+    for (int i = 0; i < lastIndex; i++)
+    {
+        if(nums[i] != nums[i + 1])
+            continue;
+
+        if(nums[i] == nums[i + 1])
+            nums[i] = nums[lastIndex] + 1;
+    }
+
+    Array.Sort(nums);
+    return k;
+}
+
+// optmized solution
+public int RemoveDuplicates(int[] nums) {
+    if(nums.Length == 0) return 0;
+
+    int res = 1; 
+    for(int i = 1; i < nums.Length; i++){
+        if(nums[i] != nums[i - 1]) {
+            nums[res] = nums[i];
+            res++;
+        }
+    }
+
+    return res;
+}
