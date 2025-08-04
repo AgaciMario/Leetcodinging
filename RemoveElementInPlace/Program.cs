53629e3a15d4904ea61569fd0118de9582810d40
Agaci Mário <agacimnm@gmail.com>
2025-08-04 14:55:49 -0300

    feat: Adicionando problema da remoção de elemento inPlace(sem criar um novo array)

---
+++ RemoveElementInPlace/Program.cs
@@ -0,0 +1,18 @@
+﻿int RemoveElement(int[] nums, int val) {
+    int k = 0;
+
+    for(int i = 0; i < nums.Length; i++) {
+
+        if(nums[i] != nums[k]) {
+            int swapValue = nums[k];
+            nums[k] = nums[i];
+            nums[i] = swapValue;
+            k++;
+        }
+
+        if(nums[k] != val)
+            k++;
+    }
+
+    return k;
+}
