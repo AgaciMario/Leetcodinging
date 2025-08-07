64044600c93e96122925afb5edb56715f65e1786
Agaci Mário <agacimnm@gmail.com>
2025-08-07 16:37:42 -0300

    Adicionando Busca de posição de inserção

---
+++ SearchInsertPosition/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Solution {
+    public int SearchInsert(int[] nums, int target) {
+        return BinarySearch(nums, target, 0, nums.Length - 1);
+    } 
+
+    public int BinarySearch(int[] nums, int target, int inf, int sup) {
+        int middle = (inf + sup) / 2;
+
+        if(nums[middle] == target) return middle;
+
+        if(inf == sup) {
+            if(target > nums[middle])
+                return inf + 1;
+            else
+                return inf;
+        }
+        else {
+            if(nums[middle] < target)
+                return BinarySearch(nums, target, middle+1, sup);
+            else
+                return BinarySearch(nums, target, inf, middle-1);
+        }
+    }
+}
---
+++ SearchInsertPosition/README.md
@@ -0,0 +1,5 @@
+### Busca de posição de inserção
+
+Dado um array ordenado de valores inteiros distintos, e um valor alvo, retorne o indice se o valor alvo for encontrado dentro do array. Caso contrário retorne o indice onde o valor alvo seria inserido no array, mantendo a ordem dos valores.
+
+o algoritmo de busca criado deve ter complexidade (n log n)
