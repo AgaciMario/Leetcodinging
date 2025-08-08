8b91f80e03566e01e670345e402e0631d78c8a25
Agaci Mário <agacimnm@gmail.com>
2025-08-08 14:35:51 -0300

    Adicionando solução final para o problema da busca do indice de inserção

--- SearchInsertPosition/Program.cs
+++ SearchInsertPosition/Program.cs
@@ -1,19 +1,31 @@
-﻿public class Solution {
+﻿namespace Program;
+
+public class Program {
+    public static void Main() {
+        Solution so = new Solution();
+        int[] arr = new int[] {1, 3, 5, 6};
+
+        so.SearchInsert(arr, 0);
+    }
+
+}
+
+public class Solution {
     public int SearchInsert(int[] nums, int target) {
         return BinarySearch(nums, target, 0, nums.Length - 1);
-    } 
+    }
 
-    public int BinarySearch(int[] nums, int target, int inf, int sup) {
+    private int BinarySearch(int[] nums, int target, int inf, int sup) {
         int middle = (inf + sup) / 2;
 
         if(nums[middle] == target) return middle;
 
-        if(inf == sup) {
-            if(target > nums[middle])
+        if(inf >= sup) 
+            // if nums at inf is less than target -> target should be inserted in a position outside of the array.
+            if(nums[inf] < target) 
                 return inf + 1;
-            else
+            else 
                 return inf;
-        }
         else {
             if(nums[middle] < target)
                 return BinarySearch(nums, target, middle+1, sup);
